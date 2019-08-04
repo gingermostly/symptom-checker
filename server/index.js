8a43882a8c96d63bf57613dc44696bbfb3cc7585
Ginger Barham <gingermostly@gmail.com>
2019-08-04 15:27:33 -0700

    setup react and basic page

--- server/index.js
+++ server/index.js
@@ -5,3 +5,8 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', express.static(path.join(__dirname, '../public')));
